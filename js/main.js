f9b25318d5710577d5d868b6db8f5ff86f4c10fb
NikitaUrb <nikita160494@gmail.com>
2023-03-13 19:54:37 +0300

    added image rendering module

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
 import { createUsersPhotosArray } from './data.js';
 
-createUsersPhotosArray();
+const photosArray = createUsersPhotosArray();
+const picturesFragment = document.createDocumentFragment();
+const pictures = document.querySelector('.pictures');
+
+const newPicture = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+photosArray.forEach(({url, description, comments, likes}) => {
+  const photo = newPicture.cloneNode(true);
+  const image = photo.querySelector('.picture__img');
+  const info = photo.querySelector('.picture__info');
+  const like = info.querySelector('.picture__likes');
+  const comment = info.querySelector('.picture__comments');
+
+  image.src = url;
+  image.alt = description;
+
+  like.textContent = likes;
+  comment.textContent = comments.length;
+
+  picturesFragment.appendChild(photo);
+})
+
+pictures.appendChild(picturesFragment);
