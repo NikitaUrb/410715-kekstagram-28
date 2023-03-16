20e3f37675a7cdefb14c85086534dbd35b623721
NikitaUrb <nikita160494@gmail.com>
2023-03-16 22:56:54 +0300

    added event deletion from body

--- js/openPicture.js
+++ js/openPicture.js
@@ -6,13 +6,16 @@ const closeButton = document.querySelector('.big-picture__cancel');
 const commentCount = document.querySelector('.social__comment-count');
 const loader = document.querySelector('.comments-loader');
 
+const close = (evt) => {
+  if (evt.key === 'Escape') {
+    document.body.classList.remove('modal-open');
+    bigPicture.classList.add('hidden');
+    document.body.removeEventListener('keydown', close);
+  }
+};
+
 const closeBigPicture = () => {
-  document.body.addEventListener('keydown', (evt) => {
-    if (evt.key === 'Escape') {
-      document.body.classList.remove('modal-open');
-      bigPicture.classList.add('hidden');
-    }
-  });
+  document.body.addEventListener('keydown', close);
 
   closeButton.addEventListener('click', () => {
     document.body.classList.remove('modal-open');
