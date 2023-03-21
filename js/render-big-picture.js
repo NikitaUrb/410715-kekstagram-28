afb83fefa0f6144eec0beda2a5d8923ebc196a39
NikitaUrb <nikita160494@gmail.com>
2023-03-21 18:56:26 +0300

    added preload comments

--- js/render-big-picture.js
+++ js/render-big-picture.js
@@ -4,16 +4,39 @@ const bigPicture = document.querySelector('.big-picture');
 const countLikes = bigPicture.querySelector('.likes-count');
 const photoImage = bigPicture.querySelector('.big-picture__img img');
 const photoTitle = bigPicture.querySelector('.social__caption');
+const loaderButton = document.querySelector('.comments-loader');
+const commentCount = document.querySelector('.social__comment-count');
 
 
 const renderComments = (comments) => {
   commentsList.innerHTML = '';
+  let min = 0;
+  let max = 5;
 
-  comments.forEach((comment) => {
+  loaderButton.addEventListener('click', () => {
+    min = max;
+    max = max + 5;
+
+    comments.slice(min, max).forEach((comment) => {
+      const newComment = commentTemplate.cloneNode(true);
+      const userPhoto = newComment.querySelector('img');
+      const userMessage = newComment.querySelector('.social__text');
+
+      commentCount.textContent = `${(max > comments.length) ? comments.length : max} из ${comments.length} комментариев`;
+      userPhoto.src = comment.avatar;
+      userPhoto.alt = comment.name;
+      userMessage.textContent = comment.message;
+
+      commentsList.appendChild(newComment);
+    });
+  });
+
+  comments.slice(min, max).forEach((comment) => {
     const newComment = commentTemplate.cloneNode(true);
     const userPhoto = newComment.querySelector('img');
     const userMessage = newComment.querySelector('.social__text');
 
+    commentCount.textContent = `5 из ${comments.length} комментариев`;
     userPhoto.src = comment.avatar;
     userPhoto.alt = comment.name;
     userMessage.textContent = comment.message;
