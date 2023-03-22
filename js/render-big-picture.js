36cc10d817329e680e0c1c87fbc42add829e126a
NikitaUrb <nikita160494@gmail.com>
2023-03-22 19:44:50 +0300

    change render comments

--- js/render-big-picture.js
+++ js/render-big-picture.js
@@ -7,6 +7,21 @@ const photoTitle = bigPicture.querySelector('.social__caption');
 const loaderButton = document.querySelector('.comments-loader');
 const commentCount = document.querySelector('.social__comment-count');
 
+const preparationComments = (min, max, comments) => {
+
+  comments.slice(min, max).forEach((comment) => {
+    const newComment = commentTemplate.cloneNode(true);
+    const userPhoto = newComment.querySelector('img');
+    const userMessage = newComment.querySelector('.social__text');
+
+    commentCount.textContent = `${(max > comments.length) ? comments.length : max} из ${comments.length} комментариев`;
+    userPhoto.src = comment.avatar;
+    userPhoto.alt = comment.name;
+    userMessage.textContent = comment.message;
+
+    commentsList.appendChild(newComment);
+  });
+}
 
 const renderComments = (comments) => {
   commentsList.innerHTML = '';
@@ -17,32 +32,10 @@ const renderComments = (comments) => {
     min = max;
     max = max + 5;
 
-    comments.slice(min, max).forEach((comment) => {
-      const newComment = commentTemplate.cloneNode(true);
-      const userPhoto = newComment.querySelector('img');
-      const userMessage = newComment.querySelector('.social__text');
-
-      commentCount.textContent = `${(max > comments.length) ? comments.length : max} из ${comments.length} комментариев`;
-      userPhoto.src = comment.avatar;
-      userPhoto.alt = comment.name;
-      userMessage.textContent = comment.message;
-
-      commentsList.appendChild(newComment);
-    });
+    preparationComments(min, max, comments);
   });
 
-  comments.slice(min, max).forEach((comment) => {
-    const newComment = commentTemplate.cloneNode(true);
-    const userPhoto = newComment.querySelector('img');
-    const userMessage = newComment.querySelector('.social__text');
-
-    commentCount.textContent = `5 из ${comments.length} комментариев`;
-    userPhoto.src = comment.avatar;
-    userPhoto.alt = comment.name;
-    userMessage.textContent = comment.message;
-
-    commentsList.appendChild(newComment);
-  });
+  preparationComments(min, max, comments);
 };
 
 
