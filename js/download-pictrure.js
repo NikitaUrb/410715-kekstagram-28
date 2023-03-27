14be864c14f1c00864eb48f213c9c806e97c7772
NikitaUrb <nikita160494@gmail.com>
2023-03-28 00:03:28 +0300

    added previews images

--- js/download-pictrure.js
+++ js/download-pictrure.js
@@ -1,5 +1,6 @@
 const downloadInput = document.querySelector('#upload-file');
 const formEdit = document.querySelector('.img-upload__overlay');
+const previewImage = document.querySelectorAll('.effects__preview');
 
 downloadInput.addEventListener('change', () => {
   const reader = new FileReader();
@@ -11,10 +12,16 @@ downloadInput.addEventListener('change', () => {
 
     reader.addEventListener('load', () => {
       const url = reader.result;
+      const img = document.querySelector('.img-upload__preview img');
+
       formEdit.classList.remove('hidden');
       document.body.classList.add('.modal-open');
-      const img = document.querySelector('.img-upload__preview img');
+
       img.src = url;
+
+      previewImage.forEach((image) => {
+        image.style.backgroundImage = `url('${url}')`;
+      });
     });
   }
 });
